2469c13ce82c4331489de4697953a15fa10ddcf8
MJ LEE <mj1201276@yahoo.com.au>
2022-03-11 00:02:42 +1000

    Updates

---
+++ precache-manifest.00201a56529bd3ba164a69ebb444475b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c775225ccf9e00568891532f12eb1299",
+    "url": "/gix-exchange/index.html"
+  },
+  {
+    "revision": "58d55194e11460e493b9",
+    "url": "/gix-exchange/static/css/2.a7a4ff3a.chunk.css"
+  },
+  {
+    "revision": "e98021665d9c257c2aa7",
+    "url": "/gix-exchange/static/css/main.509a4151.chunk.css"
+  },
+  {
+    "revision": "58d55194e11460e493b9",
+    "url": "/gix-exchange/static/js/2.88b373de.chunk.js"
+  },
+  {
+    "revision": "65a533fa5be9ace6b6237dd2015aeffd",
+    "url": "/gix-exchange/static/js/2.88b373de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e98021665d9c257c2aa7",
+    "url": "/gix-exchange/static/js/main.f1aad52c.chunk.js"
+  },
+  {
+    "revision": "f09986b79efd4f65a62e",
+    "url": "/gix-exchange/static/js/runtime-main.0b2e6ab3.js"
+  },
+  {
+    "revision": "bb3c3fdc5f4427688d48dbfc8d5874f3",
+    "url": "/gix-exchange/static/media/gix-logo2.bb3c3fdc.png"
+  },
+  {
+    "revision": "4edcc8c658fc10bdfb9024e7dd306781",
+    "url": "/gix-exchange/static/media/poi-logo.4edcc8c6.png"
+  }
+]);
